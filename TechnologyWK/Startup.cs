092f91d7f2ca265d0356d3c3fb4786ed7758bfa0
appidaniel97 <appidaniel97@gmail.com>
2022-09-19 09:14:07 -0300

    Refactor

--- TechnologyWK/Startup.cs
+++ TechnologyWK/Startup.cs
@@ -26,7 +26,6 @@ namespace TechnologyWK
 
         public IConfiguration Configuration { get; }
        
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -49,7 +48,6 @@ namespace TechnologyWK
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -61,7 +59,6 @@ namespace TechnologyWK
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             
@@ -71,13 +68,7 @@ namespace TechnologyWK
             app.UseRouting();
 
             app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
+         
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
